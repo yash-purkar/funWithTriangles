2c9a1b6d136cada72f47070189ae9e011bf5484c
yashpurkar <yashpurkar7079@gmail.com>
2022-12-05 20:01:44 +0530

    got sum of the squares

--- hypotenuse.js
+++ hypotenuse.js
@@ -1,3 +1,19 @@
 const sides = document.querySelectorAll(".side-input");
 const calculateBtn = document.querySelector("#calculate");
 const outputBox = document.querySelector("#output");
+
+
+function sumOfSquares(a, b) {
+  let sum = a * a + b * b;
+
+  console.log(sum)
+}
+function calculateHypo() {
+  sumOfSquares(Number(sides[0].value), Number(sides[1].value));
+
+  console.log(sum);
+
+
+}
+
+calculateBtn.addEventListener("click", calculateHypo);
