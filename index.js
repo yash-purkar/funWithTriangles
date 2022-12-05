c89dd194b73f65842c7ed91626689c49121c4855
yashpurkar <yashpurkar7079@gmail.com>
2022-12-05 22:47:23 +0530

    added style on isTriangle

--- index.js
+++ index.js
@@ -11,13 +11,18 @@ function sumOfAngles(angle1, angle2, angle3) {
 }
 
 function isTriangle() {
-  const sum = sumOfAngles(Number(inputs[0].value), Number(inputs[1].value), Number(inputs[2].value));
+  if (inputs[0].value.length > 0 && inputs[1].value.length > 0 && inputs[2].value.length > 0) {
+    const sum = sumOfAngles(Number(inputs[0].value), Number(inputs[1].value), Number(inputs[2].value));
 
-  if (sum === 180) {
-    outputBox.innerText = "Yayy! Its Forms a Triangle🤩"
+    if (sum === 180) {
+      outputBox.innerText = "Yayy! Its Forms a Triangle🤩"
+    }
+    else {
+      outputBox.innerText = "Ohh! Its not Forms a Triangle🤪"
+    }
   }
   else {
-    outputBox.innerText = "Ohh! Its not Forms a Triangle🤪"
+    outputBox.innerText = 'Plzz enter all the field first'
   }
 }
 
