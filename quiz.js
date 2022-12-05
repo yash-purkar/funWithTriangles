4e74e8057b1bf1af544d03b14ed8ebac5f805b8f
yashpurkar <yashpurkar7079@gmail.com>
2022-12-05 19:13:25 +0530

    got reference of elements

---
+++ quiz.js
@@ -0,0 +1,7 @@
+const quizForm = document.querySelector("#quiz-form");
+// All of the data will come in quizForm variable;
+const submitBtn = document.querySelector("submit-btn");
+const outputBox = document.querySelector("output");
+
+const answers = ["750sqft", "true", "7"];
+
