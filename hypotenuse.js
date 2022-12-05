31ece66354f6c7f23bfb1ba12493f530113c127b
yashpurkar <yashpurkar7079@gmail.com>
2022-12-05 21:25:22 +0530

    done hypotenuse

--- hypotenuse.js
+++ hypotenuse.js
@@ -4,19 +4,23 @@ const outputBox = document.querySelector("#output");
 
 
 
+
 function sumOfSquares(a, b) {
   let sum = a * a + b * b;
   return sum;
-
 }
 function calculateHypo() {
 
+  if (sides[0].value.length > 0 && sides[1].value.length > 0) {
+    let sum = sumOfSquares(Number(sides[0].value), Number(sides[1].value));
 
-  let sum = sumOfSquares(Number(sides[0].value), Number(sides[1].value));
-
-  let hypotenuse = Math.sqrt(sum).toFixed(2);
-  // console.log(hypotenuse);
-  outputBox.innerText = "Hypoteneus " + hypotenuse;
+    let hypotenuse = Math.sqrt(sum).toFixed(2);
+    // console.log(hypotenuse);
+    outputBox.innerText = "Hypoteneus " + hypotenuse;
+  }
+  else {
+    outputBox.innerText = "Pleasefill all the fields first."
+  }
 }
 
 calculateBtn.addEventListener("click", calculateHypo);
