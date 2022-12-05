0e25d33be2eb4ecefc6c20822a952cc19ce2d8b1
yashpurkar <yashpurkar7079@gmail.com>
2022-12-05 18:44:42 +0530

    got sum of the triangle

--- isTriangle.js
+++ isTriangle.js
@@ -1,3 +1,18 @@
 const inputs = document.querySelectorAll(".angle-input");
 const button = document.querySelector("#isTriangle-btn");
 const outputBox = document.querySelector("#outputBox");
+
+
+
+function sumOfAngles(angle1, angle2, angle3) {
+  const sumOfAngles = angle1 + angle2 + angle3;
+  // console.log(sumOfAngles)
+  return sumOfAngles;
+}
+
+function isTriangle() {
+  const sum = sumOfAngles(Number(inputs[0].value), Number(inputs[1].value), Number(inputs[2].value));
+  console.log(sum);
+}
+
+button.addEventListener("click", isTriangle);
