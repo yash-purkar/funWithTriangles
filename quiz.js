8f66eaacc70df53bf6641295ef75a753535d09fa
yashpurkar <yashpurkar7079@gmail.com>
2022-12-05 19:32:06 +0530

    got input values

--- quiz.js
+++ quiz.js
@@ -1,7 +1,20 @@
 const quizForm = document.querySelector("#quiz-form");
 // All of the data will come in quizForm variable;
-const submitBtn = document.querySelector("submit-btn");
-const outputBox = document.querySelector("output");
+const submitBtn = document.querySelector("#submit-btn");
+const outputBox = document.querySelector("#output");
 
-const answers = ["750sqft", "true", "7"];
+const answers = ["750sqft", "true", "7", "90°", "right angle"];
 
+function calculateScore() {
+  let score = 0;
+  let index = 0;
+  const formData = new FormData(quizForm);
+  // console.log(formData)
+  for (let data of formData.values()) {
+    // console.log(data);
+
+  }
+
+}
+
+submitBtn.addEventListener("click", calculateScore);
