77674415131db325f3eecd85b49fcd7579e81ec2
yashpurkar <yashpurkar7079@gmail.com>
2022-12-09 11:05:15 +0530

    docs: updated file

--- area.js
+++ area.js
@@ -10,15 +10,25 @@ function calculateMult(b, h) {
 }
 
 function calculateArea() {
+
   if (base.value.length > 0 && height.value.length > 0) {
-    let mul = calculateMult(Number(base.value), Number(height.value));
-    let area = mul / 2;
-    // console.log(area);
-    outputBox.innerText = `Area of a triangle is ${area.toFixed(2)}`
+
+    if (base.value <= 0 || height.value <= 0) {
+      outputBox.innerText = "All Values should be greater than 0"
+      outputBox.style.color = "red";
+    }
+    else {
+
+      let mul = calculateMult(Number(base.value), Number(height.value));
+      let area = mul / 2;
+      // console.log(area);
+      outputBox.innerText = `Area of a triangle is ${area.toFixed(2)}`
+    }
   }
   else {
     outputBox.innerText = "Plzz fill the all fields first."
   }
+
 }
 
 calculateBtn.addEventListener("click", calculateArea)
